d09dc4387a5719fc5f5c1ff5edd723013aba6187
Obed Vazquez <obed.vazquez@gmail.com>
2023-10-02 17:44:02 -0600

    initial java project setup

---
+++ src/main/java/org/white_sdev/samples/WebScrapper.java
@@ -0,0 +1,26 @@
+package org.white_sdev.samples;
+
+/**
+ * TODO: Complete class documentation
+ */
+public class WebScrapper {
+    /**
+     * Main method of the application.
+     * This is the method that will launch the main process and run the provided application.
+     * This is a special method of the language used to launch the main thread;
+     *  <a href="https://www.oracle.com/java/technologies/jpl1-building-applications.html#class"> more information</a>.
+     *
+     * @param args {@link String} Arguments provided by the caller of the application (Often omitted)
+     */
+    public static void main(String[] args) {
+		System.out.println("Start");
+    	try{
+    		//TODO: Implement operations of the method [to modify template: Properties>Live Templates>method]
+
+			System.out.println("Finish");
+
+    	} catch (Exception e) {
+    		throw new RuntimeException("Impossible to launch application ", e);
+    	}
+    }
+}
